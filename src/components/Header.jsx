a6364612c33b3ac551154a2ae27354085dafd074
Yana <yankaya15@gmail.com>
2021-01-11 21:39:01 +0300

    https://github.com/yankaya15/100lessons.git

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () =>{
+    return  <header className='header'>
+        <img src='https://www.freelogodesign.org/Content/img/logo-samples/flooop.png'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () =>{
+    return    <div className='content'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU1dFR0DR4d0wILeQRaH1oo1Dlsag-Gp2dXg&usqp=CAU'></img>
+        <div>
+            <img src='https://f1.upet.com/A_r2u6uZhnxA_x.jpg'></img>
+            description
+        </div>
+        <div>
+            My Posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div></div>
+    </div>
+}
+
+export default Profile;
